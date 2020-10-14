b72d9f428eb082768c18cdb942e6c68dcf5aa10f
jovanz10 <71841118+jovanz10@users.noreply.github.com>
2020-10-14 02:47:19 -0500

    Add files via upload

---
+++ 05-ECUACION 2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+#include <stdlib.h>
+#include <math.h>
+int main ()
+{
+     system("color AE");
+int i,n,a;
+  printf("escribe un numero \n");
+scanf("%d", &n);
+i=(n+1);
+a=(n*n);
+printf("el resultado es  \n");
+while (i>0) {
+  printf( "%d\n" ,a);
+    a=a-2;
+  i=i-1;
+ }
+ }
---
+++ 05-ecuacion 1.c
@@ -0,0 +1,23 @@
+
+
+//echo por jovany fernandez rodriguez 4 A MAAU
+
+#include<stdio.h>
+
+#include <stdlib.h>
+#include <math.h>
+int main ()
+{
+     system("color AE");
+int i,n,a;
+  printf("escribe un numero \n");
+scanf("%d", &n);
+i=(n*2+2);
+a=(n*3);
+printf("el resultado es  \n");
+while (i>0) {
+  printf( "%d\n" ,a);
+    a=a+2;
+  i=i-1;
+ }
+ }
